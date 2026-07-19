be2d88e36382e3ff26e733ea8ed159aa52dcf3c6
Heitor Quental <heitorquental321@gmail.com>
2026-07-18 23:48:52 -0300

    ajuste para vaga.py, e criação da pasta e arquivos de scraping

--- backend/app/schemas/vaga.py
+++ backend/app/schemas/vaga.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, model_validator
 from typing import Optional
 from datetime import datetime
 from app.db.models.vagas import FonteVagaEnum
@@ -6,8 +6,9 @@ from app.db.models.vagas import FonteVagaEnum
 class VagaBase(BaseModel):
     titulo_cargo: str
     empresa: str
-    cidade: str
-    estado: str
+    cidade: Optional[str] = None
+    estado: Optional[str] = None
+    localizacao: Optional[str] = None
     faixa_salarial_min: Optional[float] = None
     faixa_salarial_max: Optional[float] = None
     requisitos: Optional[dict] = None
@@ -15,6 +16,30 @@ class VagaBase(BaseModel):
     url_original: str
     ativa: bool = True
 
+    @model_validator(mode="before")
+    @classmethod
+    #Função placeholder para tratar casos onde a localização é enviada como um dict (ex: "localizacao" : "Recife, Pe")
+    def tratar_localizacao(cls, data: dict) -> dict:
+        local = data.get("localizacao")
+        if local and (not data.get("cidade") or not data.get("estado")):
+            if "," or " - " in local:
+                if "," in local:
+                    parts = local.split(",")
+                    data["cidade"] = parts[0].strip()
+                    data["estado"] = parts[1].strip() if len(parts) > 1
+                elif " - " in local:
+                    parts = local.split(" - ")
+                    data["cidade"] = parts[0].strip()
+                    data["estado"] = parts[1].strip() if len(parts) > 1
+                else:
+                    data["cidade"] = local.strip()
+                    data["estado"] = "Não informado"
+        if not data.get("cidade") and not data.get("estado"):
+            data("cidade") = "Não informado"
+            data("estado") = "Não informado"
+        return data
+
+            
 class VagaCreate(VagaBase):
     hash_deduplicacao: str
 
